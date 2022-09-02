3ed19ee7278fb785f8bcd8adbdc0ea401b11f8b2
Creeland <cree@provinsal.com>
2022-09-02 14:43:00 -0700

    chore: fix dependencies

--- apps/cree-nextjs/next.config.js
+++ apps/cree-nextjs/next.config.js
@@ -1,4 +1,9 @@
+const withTm = require("next-transpile-modules")(["react-ui"])
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const nextConfig = {
   reactStrictMode: true,
 }
+
+module.exports = withTm(nextConfig)
